48ec70a8422df417b1e3eb696a97b5576e7ed885
huangyx <605698554@qq.com>
2019-05-22 19:29:30 +0800

    forget

--- chess_generator.py
+++ chess_generator.py
@@ -45,6 +45,12 @@ def check_conflict(x, y, type, ret):
         if type<0 or type>2:
             print("Error type!!")
     return False
+    
+def seem_to_conflict(x, y, ret):
+    for [x1,y1,type1] in ret:
+        if x1==x or y1==y or x1-x==y1-y or x1-x==y-y1:
+            return True
+    return False
 
 def generator_chess(chessboard_size, label):
     ret = []
@@ -60,6 +66,9 @@ def generator_chess(chessboard_size, label):
             y = random.randint(0,chessboard_size-1)
             if [x,y] in used_chess:
                 continue
+            if not seem_to_conflict(x,y,ret): # If not seem to conflict, then drop this chess with 80%
+                if random.randint(0,9)>=2:
+                    continue
             type = random.randint(0,2)
             if check_conflict(x,y,type,ret): 
                 conflict_cnt += 1
@@ -67,7 +76,7 @@ def generator_chess(chessboard_size, label):
                 used_chess.append([x,y])
                 ret.append([x,y,type])
                 conflict_cnt = 0
-            if conflict_cnt >= 10: #If conflict, then retry, if conflict continuously ten times, over
+            if conflict_cnt >= 20: #If conflict, then retry, if conflict continuously ten times, over
                 if len(used_chess) >= chessboard_size/2:
                     break;
                 else:
@@ -117,14 +126,30 @@ def generator_chess_images(image_pools, chessboards, shape, seed, is_color):
             pick = random.randint(0, len(image_pools[index]) - 1)
             if is_color:
                 image = Image.open(image_pools[index][pick]).convert('RGB').resize(shape)
+                image_array = np.array(image).reshape((shape[0],shape[1],3))
             else:
                 image = Image.open(image_pools[index][pick]).convert('I').resize(shape)
-            image_array = np.array(image)
+                image_array = np.array(image).reshape((shape[0],shape[1],1))
             image_array = (image_array-127)*(1./128)
             data.append([chess[0],chess[1],image_array])
         ret.append(data)
     return ret
 
+def generate_whole_chessboard(image_pools, chessboards, shape, label):
+    h = shape[0]
+    w = shape[1]
+    chessboard_size = 8
+    cnt = 0
+    for chessboard in chessboards:
+        chessboard_img = Image.new('L',(w*8,h*8),color="white")
+        for chess in chessboard:
+            index = chess[2]
+            pick = random.randint(0, len(image_pools[index]) - 1)
+            image = Image.open(image_pools[index][pick]).convert('L').resize(shape)
+            chessboard_img.paste(image, box=(w*chess[0],h*chess[1],w*chess[0]+w,h*chess[1]+h))
+        chessboard_img.save('dataset/chessboards/'+label+'/'+str(cnt)+'.png')
+        cnt += 1
+    
 
 def get_chess_data(data_dir, sign_dir_lists, shape = (28, 28), min_chessboard_size = 2, max_chessboard_size = 10, tmp_file_prev = 
 None, seed = None, train_num_per_size = 500, test_num_per_size = 100, is_color = False):
@@ -143,6 +168,7 @@ None, seed = None, train_num_per_size = 500, test_num_per_size = 100, is_color =
         test_chess = generator_chess_by_max_size(min_chessboard_size, max_chessboard_size, num_per_size = test_num_per_size, label = label)
         print(train_chess)
         print(test_chess)
+        generate_whole_chessboard(train_pool, train_chess, shape, label)
         ret["train:%s" % (label)] = generator_chess_images(train_pool, train_chess, shape, seed, is_color)
         ret["test:%s" % (label)] = generator_chess_images(test_pool, test_chess, shape, seed, is_color)
 
@@ -162,6 +188,8 @@ Illegal(Conflict):
 Not too much chess...
 Stop when the first conflict encountered
 If a chess is a line or a diag, it can also be a line-diag.
+
+You cannot distinguish a chessboard without label
 '''
 
 if __name__ == "__main__":
@@ -169,10 +197,10 @@ if __name__ == "__main__":
                     sign_dir_lists = ['0', '1', '2'],\
                     shape = (28, 28),\
                     min_chessboard_size = 3,\
-                    max_chessboard_size = 6,\
+                    max_chessboard_size = 8,\
                     tmp_file_prev = "bin_chess_data",\
-                    train_num_per_size = 100, \
-                    test_num_per_size = 20, \
+                    train_num_per_size = 500, \
+                    test_num_per_size = 200, \
                     is_color = False
                     )
     
